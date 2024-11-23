2fe7f634e1e46cb9bf4b18c1f7da09a51fdbf2fa
KARTHIKEYAN N <karthikeyan435552@gmail.com>
2024-11-23 13:11:24 +0530

    Add files via upload

---
+++ HEXAWARE-CaseStudy/Car Rental System/Car_Rental_System/CAR_RENTAL/Program.cs
@@ -0,0 +1,14 @@
+﻿using CAR_RENTAL_SYSTEM;
+
+namespace CAR_RENTAL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Main m = new Main();
+           
+
+        }
+    }
+}
